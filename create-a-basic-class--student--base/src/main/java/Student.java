119991093a404df194e21ac5539a127a065f8297
Rohan Agrawal <101562278+RohanAgrawa@users.noreply.github.com>
2023-07-13 22:06:19 +0530

    Basic Java class

---
+++ create-a-basic-class--student--base/src/main/java/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    // write the code of student class here
+
+    int age;
+    String name;
+
+    void display()
+    {
+        System.out.println("My name is " + this.name +". I am " + this.age + " years old");
+    }
+
+    void sayHello(String name)
+    {
+        System.out.println(this.name + " says hello to " + name);
+    }
+
+}
